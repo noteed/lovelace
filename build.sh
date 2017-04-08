d3556a790ec60a930e34c923083d9bc41dd71011
Vo Minh Thu <thu@hypered.io>
2017-04-08 20:50:50 +0200

    Support GHC 7.10.3.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
+STACK_IMAGE=${1:-7.8.4}
+
 docker run \
   -v `pwd`/../lovelace:/home/gusdev/lovelace \
-  images.reesd.com/reesd/stack:7.8.4 \
+  images.reesd.com/reesd/stack:$STACK_IMAGE \
   cabal install lovelace/lovelace.cabal
